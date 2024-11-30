13e86150ab6ca11f7b9d906ff18b1fd143d98272
ahmfarisi <ahmfarisi@gmail.com>
2024-11-30 10:59:45 +0700

    Update login_screen.dart
    Add general theme and logo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,6 +10,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 300),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // TODO: 1. LOGO APLIKASI
+              SizedBox(
+                height: 150,
+                width: 150,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    'images/logo.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+              // TODO: 2. TEXTFIELD EMAIL DAN PASSWORD
+              // TODO: 3. TOMBOL LOGIN
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
